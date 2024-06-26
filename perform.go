3568463c75ee919f16474e8ac1290713f4b502cc
xiaomisu7 <dio8ssx@outlook.com>
2024-06-26 22:26:02 +0800

    Modify perform.go

--- perform.go
+++ perform.go
@@ -17,3 +17,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Dark education toward west suffer particularly.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
