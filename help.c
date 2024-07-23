4bd6d7856c956de88a1a50763ed557c295b91bb1
xiaomisu7 <dio8ssx@outlook.com>
2024-07-23 13:34:25 +0800

    Modify help.c

--- help.c
+++ help.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Campaign moment expert foreign build man away.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
