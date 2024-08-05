9da65b0f92335b25be0bedfc19cd76e009d99311
xiaomisu7 <dio8ssx@outlook.com>
2024-08-05 23:01:56 +0800

    Modify not.cpp

--- not.cpp
+++ not.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Author behavior candidate later.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
