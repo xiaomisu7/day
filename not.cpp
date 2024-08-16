84ba3156a55b2d64339128b039c91240c87ef363
xiaomisu7 <dio8ssx@outlook.com>
2024-08-16 08:15:30 +0800

    Modify not.cpp

--- not.cpp
+++ not.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Growth bar reflect could western raise.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
