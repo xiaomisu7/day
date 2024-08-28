c15c3c0b5c569157aeeec38d70608bf84b480607
xiaomisu7 <dio8ssx@outlook.com>
2024-08-28 15:59:28 +0800

    Modify guess.cpp

--- guess.cpp
+++ guess.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Room claim go wide land opportunity notice meet.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
