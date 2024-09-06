40cdec8148c2952f420833fa05c851520aae0b78
xiaomisu7 <dio8ssx@outlook.com>
2024-09-06 15:31:41 +0800

    Modify public.c

--- public.c
+++ public.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Mention continue next control claim suddenly.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
