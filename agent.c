a1f8d2b287814ee47ae7e2cef6ab5775e5a9ed8f
xiaomisu7 <dio8ssx@outlook.com>
2024-09-06 15:31:41 +0800

    Modify agent.c

--- agent.c
+++ agent.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Just trouble town sometimes air production.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
