f1def58aa28c965bb40af262966b36e73a3e64cd
xiaomisu7 <dio8ssx@outlook.com>
2024-09-11 13:47:52 +0800

    Modify public.c

--- public.c
+++ public.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Beat agreement hundred issue determine dinner.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
