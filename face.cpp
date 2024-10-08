e4285cc2ebe9f5a9eb9a6485158075b0f491203b
xiaomisu7 <dio8ssx@outlook.com>
2024-10-08 17:40:03 +0800

    Modify face.cpp

--- face.cpp
+++ face.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Pull term paper happy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
