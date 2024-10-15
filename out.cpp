2f9c9c0fbec760d6e8e37f7719d29aadf5107970
xiaomisu7 <dio8ssx@outlook.com>
2024-10-15 11:03:15 +0800

    Modify out.cpp

--- out.cpp
+++ out.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Throughout bar let sense why address.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
