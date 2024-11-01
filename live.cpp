77ef834a24098ec398e401d92aa23f3ba68386d4
xiaomisu7 <dio8ssx@outlook.com>
2024-11-01 12:59:46 +0800

    Modify live.cpp

--- live.cpp
+++ live.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Each trouble situation simple tell section.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
