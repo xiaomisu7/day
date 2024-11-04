7a9c2e2b6c88421f990ee5b969643272415e9965
xiaomisu7 <dio8ssx@outlook.com>
2024-11-04 12:56:31 +0800

    Modify daughter.cpp

--- daughter.cpp
+++ daughter.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Turn late fund.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
