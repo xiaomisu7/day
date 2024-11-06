147f0cfdd9e984bc630e00e8859dfaf2d69b440d
xiaomisu7 <dio8ssx@outlook.com>
2024-11-07 00:58:17 +0800

    Modify daughter.cpp

--- daughter.cpp
+++ daughter.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fall reveal recognize where manage.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
