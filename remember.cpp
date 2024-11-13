26e7f94e7f67567a5a4fde1482236614df40502a
xiaomisu7 <dio8ssx@outlook.com>
2024-11-13 14:45:39 +0800

    Modify remember.cpp

--- remember.cpp
+++ remember.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Current enough hospital.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
