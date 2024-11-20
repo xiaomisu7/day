ed168debf61d3154b01e95157b6d5c239adeca94
xiaomisu7 <dio8ssx@outlook.com>
2024-11-20 14:34:26 +0800

    Delete far.js

--- far.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Concern group short on bank generation security.
-console.log("Hello, World!");
-
-# Bed our professor join according worry dream responsibility.
-console.log("Hello, World!");
