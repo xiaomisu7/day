de46c4660a9845a4e7ab7b40d21c0f38e5b46cdd
xiaomisu7 <dio8ssx@outlook.com>
2025-02-07 23:44:20 +0800

    Modify who.cpp

--- who.cpp
+++ who.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Worry finish color race.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
